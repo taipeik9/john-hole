e521e81df2160bd337c18c107aa222dd154d4cb0
Henry Brown <henry.jy.brown@gmail.com>
2025-02-08 01:29:50 -0500

    changed base back

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://taipeik9.github.io/john-hole/",
+  base: "/john-hole/",
   plugins: [react()],
 });
