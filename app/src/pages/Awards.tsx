3f6f2336f7635bdcd89d08ae1f187d2952a7887c
Henry Brown <henry.jy.brown@gmail.com>
2025-02-07 22:50:16 -0500

    added temp awards page

---
+++ app/src/pages/Awards.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export default function Awards() {
+  return (
+    <Container>
+      <Typography variant="h3">
+        Under Development, this page is coming soon...
+      </Typography>
+    </Container>
+  );
+}
