2aeeeaa8dd8a8057fae5430ed75b32eb100f1333
Henry Brown <henry.jy.brown@gmail.com>
2025-02-07 22:50:26 -0500

    added awards and redirect

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import People from "./pages/People";
 import Nav from "./pages/Nav";
 import { Box, Typography } from "@mui/material";
 import General from "./pages/General";
+import Awards from "./pages/Awards";
 
 function App() {
   return (
@@ -18,8 +19,10 @@ function App() {
         <Typography variant="h4">John H🕳️le </Typography>
       </Box>
       <Routes>
+        <Route path="/" element={<Navigate to="general" replace />} />
         <Route path="/general" element={<General />} />
         <Route path="/people" element={<People />} />
+        <Route path="/awards" element={<Awards />} />
       </Routes>
     </BrowserRouter>
   );
